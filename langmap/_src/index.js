227a4c1c8fdf106435b59608d97e680786e6f240
rfong <rachefo@gmail.com>
2021-06-23 16:06:45 -0700

    [langmap] add hardcoded rudimentary coordinate switching onclick

--- langmap/_src/index.js
+++ langmap/_src/index.js
@@ -1,9 +1,12 @@
+var langmap = {};  // namespace
+
 document.addEventListener("DOMContentLoaded", function() {
 
   // Force canvas dimensions. It's spontaneously resizing in prod for reasons
   // I cannot fathom
   $('#skymap-canvas').width = 1101;
   $('#skymap-canvas').height = 810;
+  var glslCanvas;
 
   // Promise to fetch fragment shader text.
   const fetchShaderPromise = new Promise((resolve, reject) => {
@@ -28,22 +31,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Display ASCII and set up dependent GLSL uniforms
   function setupAscii(asciiData) {
-    console.log(asciiData);
     _.each(asciiData, function(row) {
       $('#skymap').append('<pre class="ascii-row">' + row + '</pre>');
     });
     $('#ascii-render').html("");
 
     // Set GLSL uniforms that are dependent on ASCII data.
-    glslPromise.then(function(glslCanvas) {
-      var canvas = $('#skymap-canvas')[0];
+    glslPromise.then(function(canvas) {
+      var canvasEl = $('#skymap-canvas')[0];
       // uniform vec2 u_coordDimensions
       //   dimensions of an ASCII "pixel"
-      console.log("ascii dims:", asciiData[0].length, asciiData.length);
-      glslCanvas.setUniform(
+      canvas.setUniform(
         "u_coordDimensions",
-        canvas.width * 1.0 / asciiData[0].length,
-        canvas.height * 1.0 / asciiData.length,
+        canvasEl.width * 1.0 / asciiData[0].length,
+        canvasEl.height * 1.0 / asciiData.length,
       );
     });
   }
@@ -51,8 +52,8 @@ document.addEventListener("DOMContentLoaded", function() {
   // Set up shader and return GlslCanvas object
   function setupShader(fragShader) {
     // Set up GLSL and load the fragment shader
-    var canvas = new GlslCanvas($('#skymap-canvas')[0]);
-    canvas.load(fragShader);
+    glslCanvas = new GlslCanvas($('#skymap-canvas')[0]);
+    glslCanvas.load(fragShader);
 
     // Set up uniforms.
     // GLSL canvas type conversion doesn't support integers, so all ints will
@@ -60,16 +61,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // uniform vec3 u_highlightRGB
     //   RGB value of a highlighted ASCII "pixel"
-    canvas.setUniform("u_highlightRGB", 1.0, 0.0, 0.0);
+    glslCanvas.setUniform("u_highlightRGB", 1.0, 0.0, 0.0);
 
     // Set currently highlighted coordinates.    
-    setHighlightCoords(canvas, [[44, 56], [40, 56], [33, 54]]);
+    setHighlightCoords([[44, 56], [40, 56], [33, 54]], glslCanvas);
 
-    return canvas;
+    return glslCanvas;
   }
 
   // Set highlighted coordinates on GLSL canvas.
-  function setHighlightCoords(canvas, coords) {
+  function setHighlightCoords(coords, canvas) {
+    if (canvas == undefined) { canvas = glslCanvas; }
     // uniform vec2 u_highlightCoords (implicit ints)
     //   ASCII coordinates to highlight.
     var args = ["u_highlightCoords"].concat(coords);
@@ -80,6 +82,14 @@ document.addEventListener("DOMContentLoaded", function() {
     //   u_highlightCoords is intentionally overallocated. We pass the
     //   contextual length so we'll know when to stop consuming coords.
     canvas.setUniform("u_numHighlightCoordsRaw", coords.length);
+  }
+
+  langmap.showPhoneme = function(phonemeId) {
+    if (phonemeId==1) {
+      setHighlightCoords([[44, 56], [40, 56], [33, 54]]);
+    } else {
+      setHighlightCoords([[74, 56], [70, 56], [73, 54]]);
+    }
   }
 
 }); // end document ready
