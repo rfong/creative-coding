33fa02768831d68221762a3a22085ed5c96bc644
rfong <rachefo@gmail.com>
2023-03-16 14:59:35 -0700

    add twin-tendrils (animated bezier) sketch
    twin-tendrils flavor text

---
+++ twin-tendrils/sketch.js
@@ -0,0 +1,112 @@
+// see p5.bezier reference
+// RC Creative Coding 3/16/23
+// prompt: Twins separated at birth meeting for the first time
+
+// other possible ideas to do:
+// - draw bezier swashes/flourishes in the negative spaces between the large beziers
+// - try the animation as a transform instead of a rescale?
+
+let xOff = 0;
+let yOff = 100;
+let bObject;
+let leftWidth = 20,
+    moveRight = true,
+    xInt,
+    maxFramesToTouch = 250,
+    nFramesToTouch = null;
+let numXIntervals = 10;
+let canvas;
+
+function setup() {
+  canvas = createCanvas(min(window.innerWidth, 800), 300);
+  canvas.parent("p5-canvas");
+  p5bezier.initBezier(canvas);
+
+  noFill();
+  strokeWeight(2);
+  xInt = width/numXIntervals;
+}
+
+function draw() {
+  background("#2C2A27");
+
+  xOff = xOff + 0.0025;
+  yOff = yOff + 0.003;
+  
+  // reached the middle; stay touching for a while
+  if (leftWidth >= width/2) {
+    // start touching in middle
+    if (nFramesToTouch == null) {
+      nFramesToTouch = maxFramesToTouch;
+      moveRight = null;
+    }
+    // done touching, reverse direction
+    else if (nFramesToTouch == 0) {
+      moveRight = false;
+      nFramesToTouch = null;
+      canvas.drawingContext.setLineDash([]);
+    }
+    // continue touching, count down
+    else {
+      nFramesToTouch--;
+      if (nFramesToTouch % 50 > 45) {
+        canvas.drawingContext.setLineDash([3, 10]);
+      } else {
+        canvas.drawingContext.setLineDash([]);
+      }
+    }
+  } else if (leftWidth <= 0) {
+    // reverse directions
+    moveRight = true;
+  }
+  
+  // move in current direction of motion
+  if (moveRight != null) {
+    leftWidth += (moveRight ? 1 : -1) * 0.75;
+  }
+  
+  const points = [
+    getNoisePoints(xOff, xInt, leftWidth),
+    getNoisePoints(xOff+1, xInt, leftWidth),
+    getNoisePoints(yOff+3, xInt, leftWidth),
+    getNoisePoints(yOff+5, xInt, leftWidth),
+  ];
+  const colors = [
+    "#FFCC16", "#D18130", "#FFF3B7", "#ed9600",
+  ];
+  
+  // draw left side beziers
+  for (let i=0; i<points.length; i++) {
+    stroke(colors[i]);
+    p5bezier.newBezier(points[i], "OPEN", 6);
+  }
+  
+  // draw (mirrored) right side beziers
+  for (let i=0; i<points.length; i++) {
+    stroke(colors[i]);
+    p5bezier.newBezier(mirrorPoints(points[i]), "OPEN", 6);
+  }
+}
+
+// Given a set of points on the left side of the screen,
+// mirror them horizontally across the center of the screen.
+function mirrorPoints(points) {
+  let newPoints = [];
+  for (let i=0; i<points.length; i++) {
+    newPoints.push([width-points[i][0], points[i][1]]);
+  }
+  return newPoints;
+}
+
+// generate control points at set X-intervals for the left
+// half of the screen, where the Y-values are a little off 
+// from the previous value. rescale to leftWidth
+function getNoisePoints(offset, xInterval, leftWidth) {
+  const points = [];
+  for (let pointX = 0; pointX <= width/2; pointX += xInterval) {
+    const pointY = ((noise(offset+pointX) * height) << 1) - (height >> 1);
+    points.push([pointX*leftWidth/width*2, pointY]);
+  }
+  return points;
+}
+
