0df61fc41f9ac694dd0d5b72caf293747f9f5136
rfong <rachefo@gmail.com>
2023-04-03 16:19:14 -0700

    bezier - refactor color state push/pop and presets, and html before/after

--- bezier/sketch.js
+++ bezier/sketch.js
@@ -4,70 +4,132 @@ _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
 /* p5 instance wrapper with bezier convenience functionality */
 class BezierSketch {
+
   constructor(htmlElementId, p, setupFn, drawFn) {
     this.p = p;
     this.htmlElementId = htmlElementId;
     this.controls = {};
 
-		// Set `setup` function on p5 instance and bind its scope to `this`
+    // Set `setup` function on p5 instance and bind its scope to `this`
     p.setup = () => {
       // Set up canvas and bind to specified element
       let canvas = p.createCanvas(400, 400);
       canvas.parent(htmlElementId);
       // Append empty control panel to end of container
-			this.htmlSelect().child(this.p.createDiv().class('controls'));
+      this.htmlSelect().child(this.p.createDiv().class('controls'));
       // Bind and execute additional setup functionality
-      setupFn.bind(this)();
+      setupFn.bind(this, p)();
     }
 
-		// Set `draw` function on p5 instance and bind its scope to `this`
-    p.draw = drawFn.bind(this);
+    // Set `draw` function on p5 instance and bind its scope to `this`
+    p.draw = drawFn.bind(this, p);
   }
 
-	// Bind a third-party function to `this`
-	bind(fnName, fn) {
-		this[fnName] = fn.bind(this);
-	}
+  // Bind a third-party function to `this`
+  bind(fnName, fn) {
+    this[fnName] = fn.bind(this);
+  }
 
   /* -------------------------------------------------------------------------
-	 * Controllable parameters
+   * p5 color management helpers
+   */
+
+  // If undefined, don't change anything (default to current p5 color settings)
+  colors = {
+    bezierLine: {
+      fill: null,  // noFill
+    },
+    anchorPoints: {
+      fill: null,  // noFill
+    },
+    controlPoints: {
+      fill: [255,255,255],
+      stroke: [255,255,255],
+    },
+  };
+
+  setColors(colorSettingName, fill, stroke) {
+    this.colors[colorSettingName] = {
+      fill: fill,
+      stroke: stroke,
+    };
+  }
+
+  // Run `fn` within a draw state specified in `colors`.
+  // `fn`: a function that takes a p5 instance as its only argument.
+  // `colorSettingName`: string specifying a top-level key within `this.colors`
+  // Example usage:
+  //  this.drawEnv(this.colors.bezier, function(p) {...});
+  drawEnv(colorSettingName, fn) {
+    const p = this.p;
+    p.push(); // start new draw state
+    this.fill(this.colors[colorSettingName].fill);
+    this.stroke(this.colors[colorSettingName].stroke);
+
+    fn.bind(this, p)();  // Run `fn`, letting it know about `this` and `p`
+
+    p.pop();  // finish draw state
+  }
+
+  // If defined, use `p.fill(args)`.
+  // If null, set `noFill`
+  fill(args) {
+    if (args===undefined) { return; }
+    if (args==null) { this.p.noFill(); }
+    else if (args) {
+      this.p.fill.apply(this.p, args);
+    }
+  }
+
+  // If defined, use `p.stroke(args)`
+  // If null, set `noStroke`
+  stroke(args) {
+    if (args===undefined) { return; }
+    if (args==null) { this.p.noStroke(); }
+    else if (args) {
+      this.p.stroke.apply(this.p, args);
+    }
+  }
+
+  /* -------------------------------------------------------------------------
+   * Controllable parameters
    */
 
   isDebug() { return true; } // Implement me
 
   /* -------------------------------------------------------------------------
-	 * HTML helpers
+   * HTML helpers
    */
 
-	// Select first match within the html container bound to this instance
+  // Select first match within the html container bound to this instance
   htmlSelect(id) {
-		return this.p.select(
-			'#'+this.htmlElementId + (id===undefined ? '' : ' ' + id)
-		);
-	}
+    return this.p.select(
+      '#'+this.htmlElementId + (id===undefined ? '' : ' ' + id)
+    );
+  }
 
-	// Add a p5.Element `input` of some kind to the control panel
+  // Add a p5.Element `input` of some kind to the control panel
   addControl(id, ctrlEl, label) {
-		let className = id + '-ctrl';
+    let className = id + '-ctrl';
     // Add to instance dict of controls
     this.controls[id] = ctrlEl;
     // Create html container and add to control panel
     this.htmlSelect('.controls').child(this.p.createDiv(
-			_.template(`
+      _.template(`
         <span class="{{className}}"></span>
         <label>{{label}}</label>
       `)({
         className: className,
         label: label,
       })
-		));
+    ));
     // Set parent as the html container we just created
     ctrlEl.parent(this.htmlSelect('.controls .'+className));
     return ctrlEl;
   }
 
   /* -------------------------------------------------------------------------
-	 * Convenience functions for math & drawing
+   * Convenience functions for math & drawing
    */
 
   drawWarpedLine(y,r,theta) {
@@ -139,31 +201,28 @@ class BezierSketch {
   drawBezier(p1,p2, cp1,cp2) {
     let p = this.p;
 
-    p.bezier(
-      p1.x,p1.y,
-      cp1.x,cp1.y,
-      cp2.x,cp2.y,
-      p2.x,p2.y,
-    );
+    // Visualize control points first (underneath)
+    this.drawEnv('controlPoints', function(p) {
+      this.drawPoint(cp1);
+      p.line(p1.x,p1.y,cp1.x,cp1.y);
+      
+      this.drawPoint(cp2);
+      p.line(p2.x,p2.y,cp2.x,cp2.y);
+    });
+    
+    // Draw bezier line on top
+    this.drawEnv('bezierLine', function(p) {
+      p.bezier(p1.x,p1.y, cp1.x,cp1.y, cp2.x,cp2.y, p2.x,p2.y);
+    });
     
     if (!this.isDebug()) return;
     
-    // Visualize anchor points
-    this.drawPoint(p1);
-    this.drawPoint(p2);
+    // Visualize anchor points on top
+    this.drawEnv('anchorPoints', function(p) {
+      this.drawPoint(p1);
+      this.drawPoint(p2);
+    });
     
-    // Visualize control points
-    p.push();  // start new draw state
-    p.fill(255,255,255);
-    p.stroke(255,255,255);
-    
-    this.drawPoint(cp1);
-    p.line(p1.x,p1.y,cp1.x,cp1.y);
-    
-    this.drawPoint(cp2);
-    p.line(p2.x,p2.y,cp2.x,cp2.y);
-    
-    p.pop();  // finish draw state
   }
  
   // Helper that adds a `vertex` and accepts 2d vector
@@ -199,62 +258,95 @@ class BezierSketch {
  * END BezierSketch class
  */
 
-// Factory to create a new bezier sketch attached to `htmlElementId` container
-let bezierSketchFactory = (htmlElementId) => {
+// Factory to create a new bezier sketch attached to `htmlElementId` container.
+// `setupFn` and `drawFn` both take a p5 instance as their only parameter.
+// `htmlAfter` and `htmlBefore` take paragraph content to prepend and append to 
+//   the container, respectively.
+function bezierSketchFactory(htmlElementId, setupFn, drawFn, htmlBefore, htmlAfter) {
   return new p5((p) => {
-    let sketch = new BezierSketch(htmlElementId, p, setup, draw);
-
-		// p5 setup function
-    function setup() {
-    	const PI=p.PI;
-
-      // setup controls
-      this.addControl(
-        'fit', p.createSlider(0, PI/2, PI/4, PI/180), 'Curve fit',
-      ).style('width', '100px');
-
-      this.addControl(
-        'radius', p.createSlider(5, 100, 50), 'Radius',
-      ).style('width', '100px');
-
-			this.addControl('debug', p.createCheckbox('DEBUG', true), '');
-			sketch.bind('isDebug', function() {return this.controls.debug.checked()});			
+    function wrappedSetupFn(p) {
+      setupFn.bind(this, p)();
+      // After the setup function has executed, add before & after html
+      $('#'+htmlElementId)
+        .prepend('<p>'+(htmlBefore ?? '')+'</p>')
+        .append('<p>'+(htmlAfter ?? '')+'</p>');
     }
-
-		// p5 draw function
-    function draw() {
-      p.background(0);
-
-			const w = p.width/2,
-						h = p.height/2,
-						radius = this.controls.radius.value(),
-						theta = this.controls.fit.value();
-      
-      // set (0,0) to center of canvas
-      p.translate(w,h);
-      
-      // some grid lines for reference
-      p.stroke(100,100,100);
-      p.line(-w,0,w,0);
-      p.line(-w,50,w,50);
-      p.line(-w,100,w,100);
-      p.line(-w,150,w,150);
-      
-      // draw the central sphere
-      p.noStroke();
-      p.fill(255,255,255);
-      p.circle(0,0,2*radius);
-      
-      // draw the warped lines
-      p.noFill();
-      p.stroke(0, 255, 0);
-      for (let i=0; i<3; i++) {
-        this.drawWarpedLine(20*i, radius+25*(i+1), theta);
-      }
-    }
-
+    let sketch = new BezierSketch(htmlElementId, p, wrappedSetupFn, drawFn);
   });
 };
 
-bezierSketchFactory("p5-canvas-1");
-bezierSketchFactory("p5-canvas-2");
+// Canvas 1
+bezierSketchFactory('p5-canvas-1',
+  // p5 setup function
+  function(p) {
+    const PI=p.PI;
+
+    // setup controls
+    this.addControl(
+      'fit', p.createSlider(0, PI/2, PI/4, PI/180), 'Curve fit',
+    ).style('width', '100px');
+
+    this.addControl(
+      'radius', p.createSlider(5, 100, 50), 'Radius',
+    ).style('width', '100px');
+
+    this.addControl('debug', p.createCheckbox('DEBUG', true), '');
+    this.bind('isDebug', function() {return this.controls.debug.checked()});      
+  },
+  // p5 draw function
+  function(p) {
+    p.background(0);
+
+    const w = p.width/2,
+          h = p.height/2,
+          radius = this.controls.radius.value(),
+          theta = this.controls.fit.value();
+    
+    // set (0,0) to center of canvas
+    p.translate(w,h);
+    
+    // some grid lines for reference
+    /*
+    p.stroke(100,100,100);
+    p.line(-w,0,w,0);
+    p.line(-w,50,w,50);
+    p.line(-w,100,w,100);
+    p.line(-w,150,w,150);
+    */
+    
+    // draw the central sphere
+    p.noStroke();
+    p.fill(255,255,255);
+    p.circle(0,0,2*radius);
+    
+    // draw the warped lines
+    p.noFill();
+    p.stroke(0, 255, 0);
+    for (let i=0; i<3; i++) {
+      this.drawWarpedLine(20*i, radius+25*(i+1), theta);
+    }
+  }
+);
+
+// Canvas 2
+bezierSketchFactory('p5-canvas-2',
+  // p5 setup function
+  function(p) {},
+  // p5 draw function
+  function(p) {
+    p.background(255,0,0);
+
+    p.stroke(0,0,255);
+    this.setColors('controlPoints', [0,255,0], [0,255,0]);
+    this.drawBezier(
+      new p5.Vector(100,100),
+      new p5.Vector(200,200),
+      new p5.Vector(100,150),
+      new p5.Vector(150,100),
+    );
+  },
+  // html before
+  `before`,
+  // html after
+  `after`,
+);
