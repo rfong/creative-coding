cedf2e441ae29dd61786de572f50fda35b4732b5
rfong <rachefo@gmail.com>
2023-04-04 21:24:47 -0700

    bezier - crop explorable strings to 80 chars per line in file

--- bezier/explorable.js
+++ bezier/explorable.js
@@ -18,12 +18,15 @@ p5SketchFactory('InteractiveBezierSketch', 'p5-container-1',
   // preface
   `
   Below is a single cubic Bezier curve.
-  Drag the points around to get a sense for how different configurations change the shape.
+  Drag the points around to get a sense for how different configurations 
+  change the shape.
   `,
   // caption
   `
-  The <b>"anchor points" (p0 and p1)</b> are the endpoints that the curved line must pass through.
-  <p>You can think of the <b>"control points" (cp0 and cp1)</b> as "pulling" the curve more toward them.</p>
+  The <b>"anchor points" (p0 and p1)</b> are the endpoints that the curved 
+  line must pass through.
+  <p>You can think of the <b>"control points" (cp0 and cp1)</b> as "pulling" 
+  the curve more toward them.</p>
   <p>Also notice that the control lines (white) are tangent to the curve.</p>
   `,
   {background: '#b4b4ff'},
@@ -45,7 +48,8 @@ p5SketchFactory('BezierSketch', 'p5-container-1',
     });
 
     this.defaultStyles = _.cloneDeep(this.styles);
-    // updates styles to show or hide control points & lines, based on current state of checkboxes
+    // updates styles to show or hide control points & lines, based on current 
+    // state of checkboxes
     this.bind('updateStyles', function() {
       if (this.controls.showPoints.checked()) {
         this.styles.anchorPoint = this.defaultStyles.anchorPoint;
@@ -77,9 +81,19 @@ p5SketchFactory('BezierSketch', 'p5-container-1',
     );
   },
   // preface
-  "B-curves are much more annoying to define if you're a programmer working in code instead of using a nice visual interface! Here's a simulation of my deeply frustrating experience figuring out how to build the sketches on this page.",
+  `
+  B-curves are much more annoying to define if you're a programmer working in 
+  code instead of using a nice visual interface! Here's a simulation of my 
+  deeply frustrating experience figuring out how to build the sketches on this 
+  page.
+  `,
   // caption
-  "This canvas does not have mouse interaction implemented.",
+  `
+  This canvas does not have mouse interaction implemented.
+  <p>Try changing the coordinates. But don't spend too much time bashing your 
+  head against this one, this is definitely the worst and least fun way to 
+  define a Bezier curve!</p>
+  `,
 );
 
 /* ---------------------------------------------------------------------------
@@ -96,11 +110,15 @@ p5SketchFactory('InteractiveBezierSketch', 'p5-container-2',
     ),
   ],
   // preface
-  `What if we chained together multiple cubic Beziers to make a more complex contour? Try playing with this new shape.`, 
+  `
+  1. What if we chained together multiple cubic Beziers to make a more complex 
+  contour? Try playing with this new shape.
+  `, 
   // caption
   `
-    Move the points around and try to make an elegant new shape.
-    <p>You might notice that it\'s hard to move <b>p1</b> without completely changing the character of the curve.</p>
+  Move the points around and try to make an elegant new shape.
+  <p>You might notice that it\'s hard to move <b>p1</b> without completely 
+  changing the character of the curve.</p>
   `,
   {background: '#82ace0'},
 );
@@ -115,11 +133,19 @@ p5SketchFactory('InteractiveBezierSketch', 'p5-container-2',
     ),
   ],
   // preface
-  `In this sketch, control points will follow their associated anchor points. Try moving <b>p1</b> now.`,
+  `
+  2. In this sketch, when you move an anchor point, the associated control 
+  points will follow it. Try moving <b>p1</b> now.
+  `,
   // caption
   `
-  Now that the control points move with the anchor points, it's easier to drag <b>p1</b> to make incremental tweaks to the shape without completely changing its character.
-  <p>Notice that it's still hard to move the central control points <b>cp1</b> and <b>cp2</b> without completely changing the character of the curve. This makes it hard to change the central tilt while keeping the shape smooth in the middle.</p>
+  Now that the control points move with the anchor points, it's easier to drag 
+  <b>p1</b> to make incremental tweaks to the shape without completely 
+  changing its character.
+  <p>Notice that it's still hard to move the central control points <b>cp1</b> 
+  and <b>cp2</b> without completely changing the character of the curve. This 
+  makes it hard to change the central tilt while keeping the shape smooth in 
+  the middle.</p>
   `,
   {background: '#6bd6c7'},
 );
@@ -135,11 +161,17 @@ p5SketchFactory('InteractiveBezierSketch', 'p5-container-2',
     ),
   ],
   // preface
-  `If you want the contour to stay smooth in the middle, then pairs of control lines must make a straight, continuous handle.`,
+  `
+  3. In this sketch, paired control points are locked to stay collinear with 
+  their associated anchor point. Try moving the middle control points now.
+  `,
   // caption
   `
-  In this example, the two control points on either side of <b>p1</b> are fixed to each other to form a single straight control handle. This keeps the curve smooth at <b>p1</b>.
-  <p>Note that you can still change the length of each control handle if you want to change how aggressive the curve is on either side.</p>
+  In this example, the two control points on either side of <b>p1</b> are 
+  locked to form a straight control handle that is tangent to the curve at 
+  <b>p1</b>. This keeps the curve smooth in the middle.
+  <p>Note that you can still change the length of each control handle if you 
+  want to change how aggressive the curve is on either side.</p>
   `,
   {background: '#82bf86'},
 );
